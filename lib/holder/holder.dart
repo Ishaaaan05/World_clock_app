27624278dc486d6361b317bde835c365cd314be9
Ishaaaan05 <kanadiaishan05@gmail.com>
2022-07-13 20:41:14 +0530

    My First Commit

---
+++ lib/holder/holder.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Holder extends StatefulWidget {
+  const Holder({Key? key}) : super(key: key);
+
+  @override
+  State<Holder> createState() => _HolderState();
+}
+
+class _HolderState extends State<Holder> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+    
+  }
+}
